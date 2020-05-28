90a812c855a37d95bf976c061499260cf8cc9951
2ndyrGroupC <2ndyrGroupC@pn.local>
2020-05-28 17:36:03 +0800

    index outside

--- router/router.js
+++ router/router.js
@@ -1,47 +1,46 @@
-module.exports = function (app) {
+module.exports = function(app) {
 
-	var express = require("express");
-	var router = express.Router();
+    var express = require("express");
+    var router = express.Router();
 
-	const create = require('../controller/create.js');
-	const retrieve = require('../controller/retrieve.js');
-	const update = require('../controller/update.js');
-	const deleted = require('../controller/delete.js');
+    const create = require('../controller/create.js');
+    const retrieve = require('../controller/retrieve.js');
+    const update = require('../controller/update.js');
+    const deleted = require('../controller/delete.js');
 
-	var path = __basedir + '/views/';
+    var path = __basedir;
 
-	router.use(function (req, res, next) {
-		console.log("/" + req.method);
-		next();
-	});
+    router.use(function(req, res, next) {
+        console.log("/" + req.method);
+        next();
+    });
 
-	app.get('/', (req, res) => {
-		res.sendFile(path + "index.html");
-	});
+    app.get('/', (req, res) => {
+        res.sendFile(path + "index.html");
+    });
 
-	app.get("/item/retrieve/:item", (req, res) => {
-		if (req.params.item != "all") {
-			// Retrieve  Item
-			retrieve.findId(res, req.params.item)
-		}
-		else {
-			// Retrieve all Item
-			retrieve.findAll(res)
-		}
+    app.get("/item/retrieve/:item", (req, res) => {
+        if (req.params.item != "all") {
+            // Retrieve  Item
+            retrieve.findId(res, req.params.item)
+        } else {
+            // Retrieve all Item
+            retrieve.findAll(res)
+        }
 
-	})
-	app.post('/item/create', (req, res) => {
-		create.save(req.body, res)
-	});
+    })
+    app.post('/item/create', (req, res) => {
+        create.save(req.body, res)
+    });
 
-	app.get('/item/delete/:id', (req, res) => {
-		deleted.delete(req.params.id, res)
-	});
+    app.get('/item/delete/:id', (req, res) => {
+        deleted.delete(req.params.id, res)
+    });
 
-	app.post('/item/update/:id/:content', (req, res) => {
-		console.log(req.params)
-		
-		update.update(req.params.id, req.params.content, res)
-	});
+    app.post('/item/update/:id/:content', (req, res) => {
+        console.log(req.params)
+
+        update.update(req.params.id, req.params.content, res)
+    });
 
 }
